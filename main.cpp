c2111821b8912360050f8fccf0f9c1be9170dca0
Gustavo Brunetto <gustavobruneto@gmail.com>
2017-09-23 15:54:56 -0300

    func: one client to one server

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include "Insert.h"
 #include "Remove.h"
 #include "Queues.h"
+#include "Servers.h"
 
 int main(int argc, char *argv[]) {
 
@@ -19,49 +20,65 @@ int main(int argc, char *argv[]) {
     Process *firstProcess = nullptr;
     Process *lastProcess = nullptr;
 
-    int nServers = 10;
-    int nQueues = 10;
+    int nServers;
+    int nQueues;
+
+    nServers = nQueues = 10;
+
     int nSteps = 10;
 
+    int processCreationRate = 1;
+    int processDestructionRate = 1;
 
     // Creating an array of queues
     Queues **availableQueues = new Queues *[nQueues];
     for (int i = 0; i < nQueues; ++i) {
-        availableQueues[i] = new Queues(i, 1, nullptr, nullptr);
+        availableQueues[i] = new Queues(i, processCreationRate, nullptr, nullptr);
         if (availableQueues[i] == nullptr) exit(EXIT_FAILURE);
     }
 
-    // serverRate
-    // processRate
-
-    //time(&timeStart);
+    // Creating an array of servers
+    Servers **availableServers = new Servers *[nServers];
+    for (int i = 0; i < nServers; ++i) {
+        availableServers[i] = new Servers(i, processDestructionRate, nullptr);
+    }
 
     // cada passo da simulacao (a cada "segundo")
-    for (int i = 0; i < nSteps; ++i) {
+    for (int time = 0; time < nSteps; ++time) {
+        std::cout << "******************* STEP = " << time << " *****************************" << std::endl;
 
         // Para cada uma das filas, create new processes (pode ser diferente para cada fila)
         for (int j = 0; j < nQueues; ++j) {
             // TODO: insert the creation process rate
             firstProcess = availableQueues[j]->getFirstProcess();
             lastProcess = availableQueues[j]->getLastProcess();
-            int addProcess = insert(&firstProcess, &lastProcess, i+j);
+            int addProcess = insert(&firstProcess, &lastProcess, time+j);
 
-
-
-            std::cout << "step = " << i << " - add element id = " << i+j << " at queue id = " << j << " at time = " << lastProcess->getCreationTime() << std::endl;
+            std::cout << "add element id = " << time+j << " at queue id = " << j << " at time = " << lastProcess->getCreationTime() << std::endl;
             if (addProcess == 1) {
                 availableQueues[j]->setFirstProcess(firstProcess);
                 availableQueues[j]->setLastProcess(lastProcess);
+                availableServers[j]->setProcessToServe(firstProcess);
             }
         }
-
         // Para cada um dos servidores, executa os processos correspondentes
         // server request
-        for (int k = 0; k < nServers; ++k) {
 
-            //remove(&firstProcess);
-              // pegar o tempo na remocao
+        for (int j = 0; j < nServers; ++j) {
+            // TODO: insert the server process rate
+            firstProcess = availableServers[j]->getProcessToServe();
+            //int removeProcess = remove(&firstProcess);
+            //if (removeProcess == 1) {
+                //availableQueues[j]->setFirstProcess(firstProcess);
+                //availableQueues[j]->setLastProcess(lastProcess);
+                //availableServers[j]->setProcessToServe(firstProcess);
+            //}
+            //std::cout << "server id = " << j << " removed the process id = " << removeProcess << " from queue id = " << availableQueues[j]->getId() << std::endl;
+            // pegar o tempo na remocao
         }
+
+
+
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
 
